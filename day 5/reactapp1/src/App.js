63ddd812767a569da83d0e3fe8c5e7b4738692d8
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2023-11-23 12:34:30 +0530

    Day 5

---
+++ day 5/reactapp1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Lifecycle from './component/Lifecycle';
+import FunctionalLifecycle from './component/FunctionalLifecycle';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Lifecycle/>
+      <FunctionalLifecycle/>
+    </div>
+  );
+}
+
+export default App;
