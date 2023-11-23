88037d77973c40faa3ea1d136f7318f045e2782b
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2023-11-23 16:24:55 +0530

    Day 8

---
+++ Day 8/reactapp1/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+//import ConditionalComponent from './components/ConditionalComponent';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+     
+    </div>
+  );
+}
+
+export default App;
