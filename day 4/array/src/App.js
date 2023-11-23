bc10f5f555936c8518d4639ba7104874c68a021e
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2023-11-23 12:32:54 +0530

    Day 4

---
+++ day 4/array/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MaterialUI from './component/MaterialUI';
+
+function App() {
+  return (
+    <div className="App">
+      <MaterialUI/>
+    </div>
+  );
+}
+
+export default App;
