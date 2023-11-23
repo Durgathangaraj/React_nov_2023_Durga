185e1a3c1e8a2ef5aa4862d544dcb8b4aa07f9c6
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2023-11-23 12:27:48 +0530

    Day 1

---
+++ day 1/reactapps/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import HelloClass from "./components/HelloClass";
+function App()
+{
+  return(
+    <div className="App">
+      <HelloClass/>
+    </div>
+  );
+}
+export default App;
