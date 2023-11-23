10ada25026aceb5fc65f79f545426345e168bd3c
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2023-11-23 12:35:48 +0530

    Day 6

---
+++ Day 6/reactapp1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Person1 from './component/Person1';
+import Person2 from './component/Person2';
+
+function App() {
+  return (
+    <div className="App">
+        <Person1/>
+        <Person2/>    </div>
+  );
+}
+
+export default App;
